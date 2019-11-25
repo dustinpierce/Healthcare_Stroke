3b7d6b0b03d4c072fa0f56a1a662dc1027f5cd21
97caguilar <32245770+97caguilar@users.noreply.github.com>
2019-11-25 16:24:23 -0500

    Update StrokeShiny.R
    This is the latest--need to add models to that tab as well as fix the different ggplots we want to use for experimentation of data visualization

--- StrokeShiny.R
+++ StrokeShiny.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 library(dplyr)
@@ -14,56 +5,101 @@ library(shinythemes)
 library(markdown)
 library(DT)
 
-dataset <- iris %>% rename("Sepal_Length"=Sepal.Length, "Sepal_Width"=Sepal.Width, "Petal_Length"=Petal.Length, "Petal_Width"=Petal.Width)
+#change the file directory as we go -- how to host the csv and models and images on Shiny.io ??
+dataset <- read.csv("~/Desktop/Genomics_Project-master/strokedata.csv")
+dataset <- dataset %>% select(-X)
+#should we use the cleaned model that is used for analysis, or the one that is filtered but still contains the original variables from the data dictionary for the data exploration? 
 
 ui <- navbarPage(
-        theme = shinythemes::shinytheme("slate"),
+        theme = shinythemes::shinytheme("journal"),
         "Stroke Data",
+        tabPanel("Background", 
+                 fluidRow(
+                     column(12,
+                            h3("About"),
+                            "This app is designed to investigate results through visualization, summary statistics and model evaluation. We obtained our dataset from Kaggle Healthcare Dataset Stroke Data. This dataset contains information on more than 40,000 patients along dimensions such as gender, BMI, smoke history, and more."),
+                     column(6,
+                            h4("Data Dictionary"),
+                            imageOutput("datadictionary")
+                            )
+                        )
+                    ),
         tabPanel("Data Exploration",
-                 sidebarPanel(
-                     
-                     sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
-                                 value=min(50, nrow(dataset)), step=1, round=0),
-                     
-                     selectInput('x', 'X', names(dataset)),
-                     selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
-                     selectInput('color', 'Color', c('None', names(dataset))),
-                     
-                     checkboxInput('jitter', 'Jitter'),
-                     checkboxInput('reg', 'Regression'),
-                     
-                     selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
-                     selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
-                 ),
-                 mainPanel(
                      tabsetPanel(
-                         tabPanel("Summary Stats",
-                                  h4("Table"),
-                                  tableOutput("table"),
-                                  h4("Verbatim text output"),
-                                  verbatimTextOutput("txtout"),
-                                  h1("Header 1"),
-                                  h2("Header 2"),
-                                  h3("Header 3"),
-                                  h4("Header 4"),
-                                  h5("Header 5")
+                         tabPanel("Summary Statistics",
+                                  h4("Stroke Dataset"),
+                                  dataTableOutput("table"),
+                                  h4("Summary of Variables"),
+                                  verbatimTextOutput("summary")#,
+                                  # h1("Header 1"),
+                                  # h2("Header 2"),
+                                  # h3("Header 3"),
+                                  # h4("Header 4"),
+                                  # h5("Header 5")
                          ),
-                         tabPanel("Plots", "This panel is intentionally left blank"),
-                         tabPanel("Analysis", "This panel is intentionally left blank")
+                         tabPanel("Plots", 
+                                  sidebarPanel(
+                             # sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                             #    value=min(50, nrow(dataset)), step=1, round=0),
+                             selectInput('x', 'Variables', names(dataset)),
+                             #selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+                             selectInput('fill', 'Fill', c('None', names(dataset))),
+                             
+                             checkboxInput('bar', 'Bar'),
+                             checkboxInput('hist', 'Histogram'),
+                             
+                             # selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+                             # selectInput('facet_col', 'Facet Column', c(None='.', names(dataset))),
+                             width=3),
+                             plotOutput("plot"), width = "50%"
                      )
                  )
         ),
-        tabPanel("Models", "This panel is intentionally left blank"),
-        tabPanel("Background", "This panel is intentionally left blank")
+        tabPanel("Models", "This panel is intentionally left blank")
     )
 
 server <- function(input, output) {
+    output$datadictionary <- renderImage({
+        return(list(
+            src ="~/Desktop/Genomics_Project-master/datadictionary.jpg",
+            contentType = "image/jpeg",
+            width = 600
+        ))
+    }, deleteFile = FALSE)
+    
     output$txtout <- renderText({
         paste(input$txt, input$slider, format(input$date), sep = ", ")
     })
-    output$table <- renderTable({
-        head(cars, 4)
+    output$table <- renderDataTable(
+        dataset %>% select(-id), 
+        options = list(rownames = FALSE)
+    )
+    output$plot <- renderPlot({
+        
+        p <- ggplot(dataset, aes_string(x=input$x))#, y=input$y))
+        
+        if (input$fill != 'None')
+            p <- p + aes_string(fill=input$fill)
+        
+        # facets <- paste(input$facet_row, '~', input$facet_col)
+        # if (facets != '. ~ .')
+        #     p <- p + facet_wrap(facets)
+        
+        if (input$bar)
+            p <- p + geom_bar() 
+        if (input$hist)
+            p <- p + geom_histogram()
+        
+        print(p)
+        
+    }, width = 800)
+    
+    output$summary <- renderPrint({
+        summary(dataset)
     })
+    #output$summary <- renderPrint({
+      #  summary(dataset %>% select(-X))
+   # })
 }
 
 shinyApp(ui = ui, server = server)
